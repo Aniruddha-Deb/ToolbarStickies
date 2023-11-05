04101fdeff4751512c7674c452e83aaef3d5a18d
Aniruddha Deb <aniruddha.deb.2002@gmail.com>
2023-11-05 14:39:23 +0530

    First Commit

--- ToolbarStickies/ToolbarStickiesApp.swift
+++ ToolbarStickies/ToolbarStickiesApp.swift
@@ -6,27 +6,41 @@
 //
 
 import SwiftUI
-import SwiftData
 
 @main
 struct ToolbarStickiesApp: App {
-    var sharedModelContainer: ModelContainer = {
-        let schema = Schema([
-            Item.self,
-        ])
-        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
-
-        do {
-            return try ModelContainer(for: schema, configurations: [modelConfiguration])
-        } catch {
-            fatalError("Could not create ModelContainer: \(error)")
-        }
-    }()
+    
+    @AppStorage("showMenuBarExtra") private var showMenuBarExtra = true
+    @State private var notes = ""
 
     var body: some Scene {
-        WindowGroup {
-            ContentView()
+        MenuBarExtra(
+            "App Menu Bar Extra", systemImage: "pencil",
+            isInserted: $showMenuBarExtra)
+        {
+            VStack {
+                Spacer();
+                TextEditor(text: $notes).font(.custom("menlo", size: 12));
+                HStack(spacing: 50){
+                    Button("save") {
+                        let dialog = NSSavePanel()
+                        let result = dialog.runModal()
+                        
+                        if result == .OK {
+                            let url = dialog.url!
+                            try! self.notes.write(to: url, atomically: true, encoding: .utf8)
+                        }
+                    }.buttonStyle(PlainButtonStyle()).keyboardShortcut("s", modifiers: .command) ;
+                    Button("clear") {
+                        self.notes = ""
+                    }.buttonStyle(PlainButtonStyle()).foregroundColor(.white).keyboardShortcut("w", modifiers: .command) ;
+                    Button("quit") {
+                        NSApp.terminate(nil)
+                    }.buttonStyle(PlainButtonStyle()).foregroundColor(.white).keyboardShortcut("q", modifiers: .command)
+                }
+                Spacer();
+            }.frame(width: 300, height: 300)
         }
-        .modelContainer(sharedModelContainer)
+        .menuBarExtraStyle(.window)
     }
 }
